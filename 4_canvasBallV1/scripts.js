de41bfb185c52a31b678a96698cd4a5e9d0c7dc0
limbowandering <591176276@qq.com>
2018-05-09 21:31:56 +0800

    update canvasGame

--- 4_canvasBallV1/scripts.js
+++ 4_canvasBallV1/scripts.js
@@ -15,9 +15,8 @@ window.onload = function() {
     let Ball = function(x,y,r,color){
         this.x = x;
         this.y = y;
-        // this.oldx = x;
-        // this.oldy = y;
         //这个速度回头再调整
+        //可能还需要重构一下, 用于游戏难度自定义
         this.vx = getRandom_v();
         this.vy = getRandom_v();
         this.radius = r;
@@ -29,15 +28,16 @@ window.onload = function() {
             ctx.arc(this.x, this.y, this.radius - 1, 0, Math.PI * 2);
             ctx.fillStyle = this.color;
             ctx.fill();
+            //边框就不画了. 这种canvas像素的操作,碰撞检测时候也是蛮揪心的.
             //ctx.strokeStyle = '#fff';
             //ctx.stroke();
         },
         run: function(t){
-            // this.oldx = this.x;
-            // this.oldy = this.y;
+            //目前只是匀速运动, 之前有看到过模拟重力的, 加速度什么的, 原来物理还是有用的
             this.x += this.vx * t;
             this.y += this.vy * t;
             //debugger;
+            //y/x方向上与边框的碰撞检测
             if (this.y > canvas.height - ballRadius || this.y < ballRadius) {
                 this.y = this.y < ballRadius ? ballRadius : (canvas.height - ballRadius);
                 this.vy = -this.vy;
@@ -46,6 +46,9 @@ window.onload = function() {
                 this.x = this.x < ballRadius ? ballRadius : (canvas.width - ballRadius);
                 this.vx = -this.vx;
             }
+            //接收的参数t是时间, 每t时间更新一下位置, 重新绘制一下.
+            //其实这里有问题, 往下看看requestAnimationFrame是根据显示器刷新频率来决定频率的。
+            //所以这个t 不是真的t。。。只是用来更新位置用
             this.paint();
         },
     }
@@ -74,9 +77,6 @@ window.onload = function() {
         myBall.x = event.x;
         myBall.y = event.y;
     };
-
-
-
 
     canvas.onmousedown = function(event){
         if(event.x >= (canvas.width/2)-40 && event.x <= (canvas.width/2)+60 &&
@@ -162,12 +162,6 @@ window.onload = function() {
                     let rc = Math.sqrt(Math.pow(b1.x - b2.x, 2) + Math.pow(b1.y - b2.y, 2));
                     //向上舍入
                     if (Math.ceil(rc) < (b1.radius + b2.radius + 2)) {
-                        // console.log('' + rc + ' ' + (b1.radius + b2.radius + 2));
-                        // //给与小球新的速度
-                        //
-                        // console.log(b1,b2);
-                        // window.b1 = b1; window.b2 = b2;
-                        //
 
                         let ax = ((b1.vx - b2.vx) * Math.pow((b1.x - b2.x), 2) +
                             (b1.vy - b2.vy) * (b1.x - b2.x) * (b1.y - b2.y)) / Math.pow(rc, 2);
@@ -178,9 +172,9 @@ window.onload = function() {
                         b1.vy = (b1.vy - ay) * collarg;
                         b2.vx = (b2.vx + ax) * collarg;
                         b2.vy = (b2.vy + ay) * collarg;
-                        // console.log(b1,b2);
-                        // console.log('***************');
 
+                        //这里有个坑, 如果仅仅改变速度就完蛋了, 因为再来一帧还有可能继续碰撞
+                        //所以需要立马把两个球分开
                         let clength = ((b1.radius + b2.radius + 2) - rc) / 2;
                         let cx = clength * (b1.x - b2.x) / rc;
                         let cy = clength * (b1.y - b2.y) / rc;
